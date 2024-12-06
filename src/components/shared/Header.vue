32d77ec25a7800a35d53c86df01f10a1edb55467
skvorsovoleg <o.skvortsov@logema.org>
2024-12-06 16:23:18 +0300

    Написана небольшая реализация кнопки, начал делать шапку, возникли проблемы с лого"

--- src/components/shared/Header.vue
+++ src/components/shared/Header.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { RouterLink } from "vue-router";
+import Button from "../ui/Button.vue";
 import Container from "./Container.vue";
 
 import {Heart} from "lucide-vue-next";
@@ -13,13 +14,23 @@ import {Heart} from "lucide-vue-next";
               <RouterLink class to="/">
                 <img src="#" alt="Foxbooks Logo" />
               </RouterLink>
-
+              <Button variant="outline">
+                Меню
+              </Button>
             </div>
 
             <nav class="menu">
               <ul class="menu__list">
-                <li class="menu__element"><Heart /></li>
-                <li class="menu__element">Cart</li>
+                <li class="menu__element">
+                  <Button variant="outline">
+                    <Heart/>
+                  </Button>
+                </li>
+                <li class="menu__element">
+                  <Button variant="outline">
+                    <Cart/>
+                  </Button>
+                </li>
                 <li class="menu__element">0 Р</li>
                 <li class="menu__element">Войти</li>
               </ul>
--- src/components/ui/Button.vue
+++ src/components/ui/Button.vue
@@ -1,19 +1,49 @@
 <script setup lang="ts">
+  import clsx from 'clsx';
+
   defineProps<{
-    className: String,
+    className?: String,
     variant?: String,
   }>()
-</script>
 
+</script>
 
 <template>
   <button
-      :variant="variant"
+      :variant = "variant"
+      :class = "clsx([
+          'button',
+          className,
+          {
+            ['button__secondary']: variant == 'secondary',
+            ['button__tertiary']: variant == 'tertiary',
+            ['button__ghost']: variant == 'ghost',
+            ['button__outline']: variant == 'outline'
+          }
+      ])"
   >
     <slot></slot>
   </button>
 </template>
 
-<style>
-
+<style scoped lang = "scss">
+.button{
+  border: 2px solid $black-color;
+  background-color: $black-color;
+  color: white;
+  padding: 10px 32px;
+  border-radius: 14px;
+  font-weight: 900;
+  cursor: pointer;
+  &:hover{
+    background-color: $primary-color;
+    border-color: $primary-color;
+    color: $white-color;
+  }
+  &__outline{
+    color: $black-color;
+    background-color: transparent;
+    border: 2px solid $black-color;
+  }
+}
 </style>
